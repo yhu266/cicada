e2024eec276e8d8d8bc35feb720ac96b972c9115
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-12-26 16:09:15 -0800

    reduce memory usage

--- a/sort/insertion.c
+++ a/sort/insertion.c
@@ -2,9 +2,11 @@
 #include <stddef.h>
 
 void insertionSortI32Array(int32_t* arr, const size_t n) {
+  size_t j;
+  int32_t key;
   for (size_t i = 0; i < n; ++i) {
-    size_t j = i - 1;
-    const int64_t key = arr[i];
+    j = i - 1;
+    key = arr[i];
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
       j--;
@@ -14,9 +16,11 @@ void insertionSortI32Array(int32_t* arr, const size_t n) {
 }
 
 void insertionSortI64Array(int64_t* arr, const size_t n) {
+  size_t j;
+  int64_t key;
   for (size_t i = 0; i < n; ++i) {
-    size_t j = i - 1;
-    const int64_t key = arr[i];
+    j = i - 1;
+    key = arr[i];
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
       j--;
